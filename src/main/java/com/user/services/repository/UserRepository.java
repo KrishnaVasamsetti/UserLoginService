26017454a60918eed24801e156bf6f5a353a895b
Pavana Venkata Krishna Vasamsetti <venkatakrishna.v@narayanagroup.com>
2022-11-11 17:12:25 +0530

    added missed files

--- src/main/java/com/user/services/repository/UserRepository.java
+++ src/main/java/com/user/services/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.user.services.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import com.user.services.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
